e5949f167637f2c63e2c49ea2dd7d70c2a2af5a0
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-11-11 17:16:09 +0530

    video and user model added

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose , {Schema} from "mongoose";
+
+const userSchema = new mongoose.Schema({ // new Schema()
+
+    username : {
+        type : String,
+        required : true,
+        unique : true ,
+        lowercase : true,
+        trim : true,
+        index : true
+
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true ,
+        lowercase : true,
+        trim : true,
+
+    },
+    fullname : {
+        type : String,
+        required : true ,
+        trim : true,
+        index : true
+    },
+    avatar : {
+        type : String,
+        required : true ,
+
+    },
+    coverImage : {
+        type : String ,
+        
+    },
+    watchHistory : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Video"
+        }
+    ],
+
+    password : {
+        type : String,
+        required : [true , 'password is required']
+    },
+    refreshToken : {
+        type : String
+    },
+
+
+},{timestamps});
+
+
+export const  User = mongoose.model("User", userSchema)
