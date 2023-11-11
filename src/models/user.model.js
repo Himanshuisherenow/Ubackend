dc0fe4fe0e2e83950467e91b6d5301ff69a40dfc
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-11-11 18:02:21 +0530

    jwt sign and access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose , {Schema} from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken"; // bearor token - who have this we will give access 
 
 const userSchema = new mongoose.Schema({ // new Schema()
 
@@ -52,5 +53,56 @@ const userSchema = new mongoose.Schema({ // new Schema()
 
 },{timestamps});
 
+
+userSchema.pre("save",async function(next){
+
+    if(!this.isModified("password")) return next();
+
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPassewordCorrect = async function (password){
+
+     return await bcrypt.compare(password , this.password)    
+}
+
+userSchema.methods.generateAccessToken = function(){
+
+    jwt.sign(
+        
+    {
+
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname,
+
+    },
+
+    process.env.ACCESS_TOKEN_SECRET,
+
+    {
+        expireIn : process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        
+        {
+    
+            _id : this._id,
+    
+        },
+    
+        process.env.REFRESH_TOKEN_SECRET,
+        
+        {
+            expireIn : process.env.REFRESH_TOKEN_EXPIRY
+        })
+    
+}
 
 export const  User = mongoose.model("User", userSchema)
