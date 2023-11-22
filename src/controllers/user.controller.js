378347bd9670bddf2fc519aff417d95d6120c99e
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-11-22 23:31:56 +0530

    get data from front end

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,23 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async( req, res) => {
 
 
-  return  res.status(200).json({
-        message: "ok"
-    })
+    //get user details from frontend
+    // validation not empty
+    //check if user already exist  : username : email
+    //check for images, check for avatar
+    //cloudanary upload   , avatar
+    //create user obj -  create  entry in db
+    //remove password and refresh token field  from response
+    // check user creation 
+    //return res
+
+
+    const {fullname , username , email , password } = req.body;
+    console.log(email)
+
+    
+
+
 } )
 
 
