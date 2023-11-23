bdbce6a4efa939955fa8bbf6f14b15ed3c640de0
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-11-23 14:17:26 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,8 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js ";
+import  { User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiRespons.js";
 
 const registerUser = asyncHandler(async( req, res) => {
 
@@ -17,10 +21,63 @@ const registerUser = asyncHandler(async( req, res) => {
     const {fullname , username , email , password } = req.body;
     console.log(email)
 
+    if(
+        [fullname , username , email , password ].some((field)=>
+        field?.trim() === "")
+        
+     ){
+
+        throw new ApiError ( 400 , "All field are required")
+    }
+
+  const exitstedUser = User.findOne({
+
+        $or: [{ username },{ email }]
+    })
+
+    if(exitstedUser){
+        throw new ApiError(409,"user with this email or username is exists")
+    }
+
+
+    const avatarLocalPath =    req.files?.avatar[0]?.path;
+    const coverImageLocalPath =   req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
     
+   const avatar = await uploadOnCloudinary(avatarLocalPatha);
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+   if(!avatar){
+    throw new ApiError(400,"Avatar file is required")
+   }
+
+  const user = await  User.create({
+    fullname ,
+    avatar : avatar.url,
+    coverImage : coverImage?.url||"",
+    email,
+    password,
+    username : username.toLowerCase()
+   })
 
 
+  const createdUser = await User.
+  findById(user._id).
+  select("-password -refreshToken")
+
+  if(!createdUser){
+    throw new ApiError("500","something went wrong while registering the user")
+  }
+
+
+  return res.status (201).json(
+     new ApiResponse(200 ,createdUser,"user registered succses fully")
+  )
 } )
+
 
 
 export {registerUser}
